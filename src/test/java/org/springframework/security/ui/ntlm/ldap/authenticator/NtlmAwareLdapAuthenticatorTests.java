777ed8aeb5b67e8bea7613a11d3ee6da13f9a81a
Edouard DE OLIVEIRA <doe_wanted@yahoo.fr>
2013-10-29 11:12:26 +0100

    Reworked the test case for simplification of use
    Should be clearer how to use it mainly for the auth string
    domain;user:password

--- src/test/java/org/springframework/security/ui/ntlm/ldap/authenticator/NtlmAwareLdapAuthenticatorTests.java
+++ src/test/java/org/springframework/security/ui/ntlm/ldap/authenticator/NtlmAwareLdapAuthenticatorTests.java
@@ -1,51 +1,67 @@
 package org.springframework.security.ui.ntlm.ldap.authenticator;
 
+import jcifs.smb.NtlmPasswordAuthentication;
+
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import org.springframework.ldap.core.support.LdapContextSource;
 import org.springframework.security.authentication.BadCredentialsException;
 import org.springframework.security.ldap.DefaultSpringSecurityContextSource;
+import org.springframework.security.ldap.search.FilterBasedLdapUserSearch;
 import org.springframework.security.ui.ntlm.NtlmUsernamePasswordAuthenticationToken;
-import org.springframework.ldap.core.DirContextAdapter;
-import org.springframework.ldap.core.DirContextOperations;
-
-import jcifs.smb.NtlmPasswordAuthentication;
-import org.junit.Test;
 
 /**
  * @author Luke Taylor
  * @author Alois Cochard
- * @version $Id: NtlmAwareLdapAuthenticatorTests.java 2267 2007-11-24 20:13:29Z luke_t $
+ * @author Edouard De Oliveira
  */
 public class NtlmAwareLdapAuthenticatorTests {    
-    /**
+    
+	private static final String USER_INFO="domain;user:password";
+	private static LdapContextSource ctx;
+	
+	private static NtlmAwareLdapAuthenticator authenticator;
+	
+	@BeforeClass
+	public static void init() throws Exception
+	{
+		// User used to connect and search through ldap
+		ctx = new DefaultSpringSecurityContextSource("ldap://domainController:389/");
+    	ctx.setUserDn("cn=ldapUser,cn=Users,dc=domain,dc=loc");
+    	ctx.setPassword("******");
+    	ctx.afterPropertiesSet();
+	}
+	
+	@Before
+	public void initTest()
+	{
+		authenticator = new NtlmAwareLdapAuthenticator(ctx);
+		
+		// add filter if necessary
+        FilterBasedLdapUserSearch filter = new FilterBasedLdapUserSearch("dc=domain,dc=loc", "(sAMAccountName={0})", ctx);
+        filter.setSearchSubtree(true);
+        authenticator.setUserSearch(filter);
+	}
+	
+	/**
      * See SEC-609.
      */
     @Test(expected = BadCredentialsException.class)
-    public void unauthenticatedTokenIsRejected() {
-        NtlmAwareLdapAuthenticator authenticator = new NtlmAwareLdapAuthenticator(
-                new DefaultSpringSecurityContextSource("ldap://blah"));
-
+    public void unauthenticatedTokenIsRejected()
+    {
         NtlmUsernamePasswordAuthenticationToken token = new NtlmUsernamePasswordAuthenticationToken(
-                new NtlmPasswordAuthentication("blah"), false);
+                new NtlmPasswordAuthentication("domain;user:fakepwd"), false);
         token.setAuthenticated(false);
 
         authenticator.authenticate(token);
     }
 
     @Test
-    public void authenticatedTokenIsAccepted() {
-        NtlmAwareLdapAuthenticator authenticator = new NtlmAwareLdapAuthenticator(new DefaultSpringSecurityContextSource("ldap://blah")) {
-            // mimic loading of user
-            protected DirContextOperations loadUser(String aUserDn, String aUserName) {
-                return new DirContextAdapter();
-            }
-        };
-
-        authenticator.setUserDnPatterns(new String[] {"somepattern"});
-
+    public void authenticatedTokenIsAccepted() throws Exception {
         NtlmUsernamePasswordAuthenticationToken token = new NtlmUsernamePasswordAuthenticationToken(
-                new NtlmPasswordAuthentication("blah"), false);
+                new NtlmPasswordAuthentication(USER_INFO), true);
 
         authenticator.authenticate(token);
     }
-
-
 }
